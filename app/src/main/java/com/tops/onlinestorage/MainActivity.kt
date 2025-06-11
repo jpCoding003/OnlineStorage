b3bd1e933617c76039311e992411147a14de20be
jpCoding003 <jpcoding003@gmail.com>
2025-06-11 19:36:15 +0530

    API..

---
+++ app/src/main/java/com/tops/onlinestorage/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.tops.onlinestorage
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import okhttp3.Request
+
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        getdata()
+    }
+
+    private fun getdata(){
+
+    val client = okhttp3.OkHttpClient()
+
+         var request: Request = Request.Builder().url("https://api.restful-api.dev/objects").build()
+
+        client.newCall(request).execute().use { response ->
+            val data = response.body!!.string()
+            Log.i(TAG, data)
+        }
+
+    }
+}
