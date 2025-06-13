4c94455aa6d852a2d824f1c61307934152886d87
jpCoding003 <jpcoding003@gmail.com>
2025-06-13 23:29:12 +0530

    Data Fetched from API..

--- app/src/main/java/com/tops/onlinestorage/MainActivity.kt
+++ app/src/main/java/com/tops/onlinestorage/MainActivity.kt
@@ -6,7 +6,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tops.onlinestorage.model.Product
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.Request
+import org.json.JSONArray
+import java.util.ArrayList
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
@@ -19,19 +24,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        getdata()
+
     }
 
-    private fun getdata(){
+    private suspend fun getdata(): String? = withContext(Dispatchers.IO){
 
-    val client = okhttp3.OkHttpClient()
+        // Created Client for API OkHttp network operation
+        // Perform network operation here
+        val client = okhttp3.OkHttpClient()
 
-         var request: Request = Request.Builder().url("https://api.restful-api.dev/objects").build()
+        // A reguest Builder created to requst data(To Get Data From API) from the url OR API
+        var request: Request = Request.Builder().url("https://api.restful-api.dev/objects").build()
 
-        client.newCall(request).execute().use { response ->
-            val data = response.body!!.string()
-            Log.i(TAG, data)
+        val response = client.newCall(request).execute()
+        if (response.isSuccessful){
+            // we use !! to tell compiler that the varaiable is
+            // non-nullable type, even if it's been declared as nullable
+            val jsonResponse = response.body!!.toString()
+
+            jsonResponse.let {
+                val jsonArray = JSONArray(it)
+                // Create list to store data in array
+                val productlist = arrayListOf<Product>()
+                for (i in 0 until jsonArray.length()){
+                    val jsonObject = jsonArray.getJSONObject(i)
+                    val objectID = jsonObject.getInt("id")
+                    val objectName = jsonObject.getString("name")
+
+                    //Product is objectData in the respone.body
+                    // Here "name" & "id" are Objects of data present in response.body  OR can say The data in API
+                    val product = Product(objectID, objectName)
+                    productlist.add(product)
+                }
+                return@withContext jsonResponse
+            }
+        }else{
+            null
         }
-
     }
 }
---
+++ app/src/main/java/com/tops/onlinestorage/model/Product.kt
@@ -0,0 +1,5 @@
+package com.tops.onlinestorage.model
+
+import androidx.lifecycle.LiveData
+
+data class Product(val id: Int, val name: String)
